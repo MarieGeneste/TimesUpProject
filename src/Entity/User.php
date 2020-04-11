43cea52d6de65051aba79f3c8b25ed6887ea26eb
Marie Geneste <marie.geneste@orange.fr>
2020-04-11 13:44:58 +0200

    Authentification

---
+++ src/Entity/User.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"email"}, message="Il existe déja un compte avec cette adresse email")
+ * @UniqueEntity(fields={"username"}, message="Ce nom d'utilisateur n'est pas disponible")
+ */
+class User implements UserInterface
+{
+    /**
+     * @var \Ramsey\Uuid\UuidInterface
+     * 
+     * @ORM\Id
+     * @ORM\Column(type="uuid",unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     * @Assert\Length(min=8, minMessage="Votre mot de passe doit contenir au minimum 8 caractères")
+     */
+    private $password;
+
+    /**
+     * @Assert\NotBlank(message="Vous devez saisir un mot de passe", allowNull=false)
+     * @Assert\Length(min=8, minMessage="Votre mot de passe doit contenir au minimum 8 caractères")
+     */
+    private $plain_password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="plain_password", message="Le mot de passe de confirmation doit être identique à votre mot de passe")
+     */
+    private $confirm_password;
+
+
+    /**
+     * @return \Ramsey\Uuid\UuidInterface
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    // /**
+    //  * A visual identifier that represents this user.
+    //  *
+    //  * @see UserInterface
+    //  */
+    // public function getUsername(): string
+    // {
+    //     return (string) $this->email;
+    // }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getConfirmPassword(): ?string
+    {
+        return $this->confirm_password;
+    }
+
+    public function setConfirmPassword(?string $confirm_password): self
+    {
+        $this->confirm_password = $confirm_password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plain_password;
+    }
+
+    public function setPlainPassword(?string $plain_password): self
+    {
+        $this->plain_password = $plain_password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+}
